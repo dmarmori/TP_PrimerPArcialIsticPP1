41afc35deeaee6cf2059e5117a1f47abc31ce102
dmarmori <54649635+dmarmori@users.noreply.github.com>
2019-09-25 20:49:24 -0300

    Se agrego Facturar
    Agregamos el Facturar.php

---
+++ Marmori.Dionel/Facturar.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="img/IconoEsta.png" sizes="16x16" type="image/png">
+    
+    <title>Marmori Estacionamientos</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/sticky-footer-navbar.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <header>
+      <!-- Fixed navbar -->
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="index.php">Inicio</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="Registro.php">Registro<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="Login.php">Ingresar</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link disabled" href="MasInfo.php" target="_blank">Mas info</a>
+            </li>
+          </ul>
+          <form class="form-inline mt-2 mt-md-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Buscar" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+          </form>
+        </div>
+      </nav>
+    </header>
+
+    <!-- Begin page content -->
+    <main role="main" class="container">
+      <h1 class="mt-5"style="color:hsl(0,100%,50%);"><em>Marmori Estacionamientos SA<em></h1>
+      <p class="lead"><h2>Primer sistema de estacionamiento con cobro digital</h2></p>
+      <p><h4>"El arte de estacionar"</h4></p>
+    </main>
+    <center>
+     <img src="https://www.reporteindigo.com/wp-content/uploads/2018/07/920_NOMICS_1531_30-660x437.jpg"height=350” width=1000”>
+     </center>
+    <footer class="footer">
+      <div class="container">
+        <span class="text-muted">Te interesaria<a href="TrabajaCN.php" target="_blank"> trabajar con nosotos? </a>.Gran oportunidad de desarrollo.</span>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
