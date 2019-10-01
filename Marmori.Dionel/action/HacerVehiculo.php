2ffbe8e9d2f5250e7327b16f3a1a777cd38f0b1e
dmarmori <54649635+dmarmori@users.noreply.github.com>
2019-10-01 17:36:09 -0300

    Hora de ingreso en segundos

--- Marmori.Dionel/action/HacerVehiculo.php
+++ Marmori.Dionel/action/HacerVehiculo.php
@@ -1,14 +1,15 @@
 <?php
 	$miobjetoVehiculo=new stdClass();
 
-	$IngresoHora = time();
+	//$IngresoHora = time();
 	date_default_timezone_set('America/Argentina/Buenos_Aires');
-	$IngresoHora = date ('H:i:s',$IngresoHora);
+	$IngresoHora = mktime(); 
+	//$IngresoHora = date ('H:i:s',$IngresoHora);
 
 	$miobjetoVehiculo->Patente = $_GET['Patente'];
 	$miobjetoVehiculo->Horario = $IngresoHora;
 
-	$archivo=fopen('Vehiculo.txt','a');
+	$archivo=fopen('Vehiculos.txt','a');
 	fwrite($archivo,json_encode($miobjetoVehiculo)."\n");
 	fclose($archivo);
 
